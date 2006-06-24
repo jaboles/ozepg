bbc013ed944dd867f7c7bfb5b9bf92023b0d7416
jb <jb@ad6e6e65-b337-8b41-baeb-31f2621532fc>
2006-06-24 15:41:58 +0000

    Added a left-out file from previous revision.
    git-svn-id: https://jbpc/svn/ozepg@7 ad6e6e65-b337-8b41-baeb-31f2621532fc

---
+++ XMLTVDocument.java
@@ -0,0 +1,47 @@
+//
+//  XMLTVDocument.java
+//  ozEPG
+//
+//  Created by Jonathan Boles on 25/06/06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+package jb.ozepg;
+import java.util.*;
+import java.io.*;
+import org.jdom.*;
+import org.jdom.output.*;
+
+public class XMLTVDocument {
+	private ArrayList programmeList;
+	private ArrayList channelList;
+	
+	public XMLTVDocument() {
+		channelList = new ArrayList();
+		programmeList = new ArrayList();
+	}
+	
+	public void addProgramme(Programme p) {
+		programmeList.add(p);
+	}
+	
+	public Document toXMLDocument() {
+		Document doc = new Document();
+		doc.setRootElement(toXMLElement());
+		
+		XMLOutputter xo = new XMLOutputter(Format.getPrettyFormat());
+		try {
+			xo.output(doc, System.out);
+		} catch (IOException e) {};
+		
+		return doc;
+	}
+	
+	public Element toXMLElement() {
+		Element e = new Element("tv");
+		
+		for (int i = 0; i < programmeList.size(); i++)
+			e.addContent(((Programme)programmeList.get(i)).toXMLElement());
+		
+		return e;
+	}
+}
